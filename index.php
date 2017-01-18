cc4fcdcb57ebf9c29a581ea75dc4da7143ea00f4
vomenthan <thancn12@gmail.com>
2017-01-18 17:26:42 +0700

    no message

--- index.php
+++ index.php
@@ -35,5 +35,22 @@
     <div class="img-cut">
         <span class='img_facebook logo icon'></span>
     </div>
+<!--    from drag drop file-->
+    <form class="box" method="post" action="" enctype="multipart/form-data">
+        <div class="box__input">
+            <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
+            <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
+            <button class="box__button" type="submit">Upload</button>
+        </div>
+        <div class="box__uploading hid">Uploading&hellip;</div>
+        <div class="box__success hid">Done!</div>
+        <div class="box__error hid">Error! <span></span>.</div>
+    </form>
+<!--end drag drop file-->
+
+
+
+
+
 </article>
 <?php include("footer.php"); ?>
